99b5109bb6167351ec8a8116d303763b0ff147ed
Lorena <lorena.del88@hotmail.com>
2022-02-16 12:39:45 +0100

    switch con las notas

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log("¿Estoy aprobado?");
+
+var notas = prompt('¿has aprovado?')
+
+switch (nota) {
+    case 0 :
+    case 1 :
+    case 2 :
+    case 3 :
+    case 4 :
+    console.log("Suspenso");
+break;
+    case 5 :
+    case 6 :
+    console.log("Suficiente");
+break;
+    case 7 : 
+    case 8 :
+    console.log("Notable");
+break;       
+    case 9 :
+    case 10 :
+    console.log("Sobresaliente");
+break;
+}
